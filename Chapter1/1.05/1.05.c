8591b0307a1afd6faa6848448ea24cd6b123f622
Steven <e0390018@u.nus.edu>
2022-02-21 18:03:24 +0800

    pratice

---
+++ Chapter1/1.05/1.05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float celsiusToFehr(float celsius);
+
+int main(){
+    int celsius, fahr;
+    printf("%7s\t%8s\n","celsius","fahr");
+    for (int i = 300; i >= 0; i -= 20){
+        printf("%7d\t%8.2f\n", i, celsiusToFehr(i));
+    }
+}
+
+float celsiusToFehr(float celsius){
+    return celsius * (9.0/5.0) + 32.0;
+}
