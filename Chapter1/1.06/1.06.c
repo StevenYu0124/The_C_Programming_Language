c959e3196ed2db04ab88c30b5dd815b7d646fc7a
Steven <e0390018@u.nus.edu>
2022-02-21 18:40:03 +0800

    pratice

---
+++ Chapter1/1.06/1.06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define INPUT_FILE "shortInput" 
+
+int main(){
+    int c;
+    FILE *input;
+    
+    input = fopen(INPUT_FILE, "r");
+    if (input == 0){
+        printf("error opening the file:%s", INPUT_FILE);
+        return -1;
+    }
+
+    do{
+        c = fgetc(input);
+        printf("getchar() != EOF return %d\n",c != EOF);
+        putchar(c);
+        printf("\n");
+    }
+    while (c != EOF);
+    printf("\n");
+}
